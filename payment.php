450a2e6765f51426aa8cb5f1ccaa9365c281c2a5
kadian-developer <sanvishivangi@gmail.com>
2021-05-26 10:51:19 +0530

    new setup

---
+++ payment.php
@@ -0,0 +1,47 @@
+Dragonpay
+https://packagist.org/packages/coreproc/dragonpay-sdk
+https://github.com/crazymeeks/dragonpay
+
+paypal
+https://www.youtube.com/watch?v=WOsXayVmnoc
+https://www.paypal.com/in/home
+https://www.positronx.io/how-to-integrate-paypal-payment-gateway-in-laravel/
+https://www.codecheef.org/article/how-to-integrate-paypal-payment-gateway-in-laravel
+
+Gcash
+https://www.gcash.com/get-star	ted/
+https://stackoverflow.com/questions/66828861/is-there-any-api-integration-for-gcash-payment
+https://docs.adyen.com/payment-methods/gcash/api-only?tab=codeBlockapi_only_payments_1
+https://docs.adyen.com/payment-methods/gcash
+
+paymaya
+https://www.paymaya.com/quick-guide/create-account
+https://github.com/aceraven777/laravel-paymaya
+https://packagist.org/packages/paymaya/paymaya-sdk
+
+paymongo
+https://dashboard.paymongo.com/signup
+https://packagist.org/packages/luigel/laravel-paymongo
+https://github.com/luigel/laravel-paymongo
+https://developers.paymongo.com/docs
+
+Online Banking
+https://github.com/ambrosethebuild/online-banking
+
+Cart functionality
+https://salitha94.blogspot.com/2019/11/create-shopping-cart-with-laravel-6-part-1.html
+
+vimeo
+https://www.youtube.com/watch?v=ovHVDVuQzX8
+
+https://vimeo.zendesk.com/hc/en-us/articles/360040771772-Enabling-disabling-and-moderating-chat
+https://help.livestream.com/hc/en-us/articles/360002069027-How-Do-I-Moderate-the-Chat-on-My-Events-
+
+https://github.com/vimeo/laravel
+access token generate in vimeo with upload scope?
+
+https://developer.vimeo.com/api/live/events
+
+
+
+--https://help.dragonpay.ph/account.php?do=create
